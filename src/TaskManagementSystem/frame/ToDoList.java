b557ab7b152b286e7c519606774fb69c9b2e0b8a
Pikott143 <Theunseenmaiden@gmail.com>
2023-12-10 19:16:45 +0800

    nag lisod nako og ginhawa unta mao na gyud ni

---
+++ src/TaskManagementSystem/frame/ToDoList.java
@@ -0,0 +1,16 @@
+package TaskManagementSystem.frame;
+
+/**
+ *
+ * @author Aj Cervantes
+ */
+import javax.swing.JFrame;
+
+public class ToDoList {
+	
+	public static void main(String args[])
+	{
+		frame frame = new frame();
+		
+	}
+}
---
+++ src/TaskManagementSystem/frame/override.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TaskManagementSystem.frame;
+
+/**
+ *
+ * @author Aj Cervantes
+ */
+
+    public @interface override {
+
+}
+
